3c73e40448226b456b44709d8b64871249aae810
Mahdi Arezoumandi <center328.a@gmail.com>
2020-03-17 08:37:13 +0330

    fix bugs

--- src/main/java/com/example/calculator/Calculator.java
+++ src/main/java/com/example/calculator/Calculator.java
@@ -1,13 +1,13 @@
 package com.example.calculator;
 
 
-import org.springframework.cache.annotation.Cacheable;
+//import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 /** Calculator logic */
 @Service
 public class Calculator {
-    @Cacheable("sum")
+//    @Cacheable("sum")
     public int sum(int a, int b) {
         return a + b;
     }
